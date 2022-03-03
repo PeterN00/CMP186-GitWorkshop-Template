4de90dbe5a5beae3354ef4f85dc36e31c03e37ae
PeterN00 <nguyentanphuc03092000hiie@gmail.com>
2022-03-03 08:48:52 +0700

    Update EmployeeService.cs

--- GitWorkshopProject/SalaryCalculator/EmployeeService.cs
+++ GitWorkshopProject/SalaryCalculator/EmployeeService.cs
@@ -15,21 +15,6 @@ namespace SalaryCalculator
 
             decimal factor = 0;
 
-            // if (employeeType == EmployeeType.Engineer)
-            // {
-            //     factor = 1;
-            // }
-            // else if (employeeType == EmployeeType.SeniorEngineer || employeeType == EmployeeType.Manager)
-            // {
-            //     factor = 1.5;
-            // }
-            // else if (employeeType == EmployeeType.SeniorManager)
-            // {
-            //     factor = 3;
-            // }
-            
-            // TODO: if employee is Expert, set factor = 3
-
             switch(EmployeeType){
                 case EmployeeType.Engineer:
                     factor = 1;
